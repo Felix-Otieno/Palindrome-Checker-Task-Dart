ac6d2d393b9cb5493febff1f7a3bd681d39c19c6
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-10-29 11:51:34 +0300

    initial commit with palindrome.dart

--- palindrome.dart
+++ palindrome.dart
@@ -1,19 +1,21 @@
-
 bool palindrome(String str) {
   // Convert the string to lowercase and filter out non-alphanumeric characters
   String character = str.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
-  
+
   // Reverse the filtered string
   String characterReversed = character.split('').reversed.join('');
-  
+
   // Print the original and reversed strings
   print("Forward: $character = Backward: $characterReversed");
-  
+
   // Check if the filtered string is equal to its reverse
   return character == characterReversed;
 }
 
 void main() {
-  bool isPalindrome = palindrome("exe");
-  print("Is the input a palindrome? $isPalindrome");
+  bool isPalindrome = palindrome("101");
+  bool isPalindrome2 = palindrome("exe");
+
+  print(isPalindrome);
+  print(isPalindrome2);
 }
