121e5bcb0e7068676d8f17e30e23f1cdd8b6074e
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-10-29 16:34:55 +0300

    initial commit with palindrome.dart

--- palindrome.dart
+++ palindrome.dart
@@ -1,4 +1,9 @@
 bool palindrome(String str) {
+  /* Declared a function called palindrome and its parameter is str.
+     The function will be used to determine if a string is a palindrome or not.
+     Palindrome is a sequence of characters chat check if the character is same forward and backward.
+   */
+  
   // Convert the string to lowercase and filter out non-alphanumeric characters
   String character = str.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
 
@@ -13,9 +18,11 @@ bool palindrome(String str) {
 }
 
 void main() {
+  // Invoke the function 
   bool isPalindrome = palindrome("101");
   bool isPalindrome2 = palindrome("exe");
 
+// Display the output to the console
   print(isPalindrome);
   print(isPalindrome2);
 }
